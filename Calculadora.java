46f3456acbb1585612729493ef5523cf790c3a2d
Gustavo <gustavo11ramossantos@gmail.com>
2024-08-17 12:25:07 -0300

    Praticas de POO

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+
+    private float a;
+    private float b;
+
+    public float getA() {
+        return a;
+    }
+
+    public float getB() {
+        return b;
+    }
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculadora cal = new Calculadora();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro valor");
+
+        System.out.println("Digite o segundo valor");
+
+    }
+}
